804fca682a5bcc1e494ce05d3915b01b65601722
kiruthikathangaraj2507 <pandakutty257@gmail.com>
2026-04-20 19:25:48 +0530

    Time: 7 ms (99.68%), Space: 12.5 MB (60.02%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.py
@@ -0,0 +1,28 @@
+class Solution:
+    def solveNQueens(self, n):
+        result = []
+        board = [["."] * n for _ in range(n)]
+        cols = set()
+        diag1 = set()  # r - c
+        diag2 = set()  # r + c
+        def backtrack(r):
+            if r == n:
+                copy = ["".join(row) for row in board]
+                result.append(copy)
+                return
+            for c in range(n):
+                if c in cols or (r - c) in diag1 or (r + c) in diag2:
+                    continue
+                # place queen
+                board[r][c] = "Q"
+                cols.add(c)
+                diag1.add(r - c)
+                diag2.add(r + c)
+                backtrack(r + 1)
+                # remove queen (backtrack)
+                board[r][c] = "."
+                cols.remove(c)
+                diag1.remove(r - c)
+                diag2.remove(r + c)
+        backtrack(0)
+        return result
